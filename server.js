5391d867a148b1027c4f781e97c14d016b7747fc
Adsana <adsanasayasith@gmail.com>
2020-09-27 21:40:21 -0400

    deploy

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require('dotenv').config()
+
 
 const PORT = 3000;
 
